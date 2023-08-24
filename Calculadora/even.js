8ff5ed8e7becc9e651bb014c4e1d92286cd63828
Selvin <selvingts20@gmail.com>
2023-08-24 00:06:18 -0600

    cambio de colores y toma de datos de inputs

---
+++ Calculadora/even.js
@@ -0,0 +1,15 @@
+'use strict'
+// Variables numeros
+const num1 = document.querySelector('input[value="1"]');
+const num2 = document.querySelector('input[value="2"]');
+const num3 = document.querySelector('input[value="3"]');
+const num4 = document.querySelector('input[value="4"]');
+const num5 = document.querySelector('input[value="5"]');
+const num6 = document.querySelector('input[value="6"]');
+const num7 = document.querySelector('input[value="7"]');
+const num8 = document.querySelector('input[value="8"]');
+const num9 = document.querySelector('input[value="9"]');
+const num0 = document.querySelector('input[value="0"]');
+//Variables signos
+const mas = document.querySelector('input[value="+"]');
+
